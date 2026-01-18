b17d837cc360e025a4a713fc90ae8d3c1b7c311f
luanrizardi <103271388+luanrizardi@users.noreply.github.com>
2026-01-18 18:25:52 +0000

    finish `api/v1/status /endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updateAt = new Date().toISOString();
+
+  // 1. Versão do Postgres
+  const databaseVersionResult = await database.query("SHOW server_version;");
+
+  // 2. Máximo de conexões permitidas
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnectionsResult.rows[0].max_connections,
+        ),
+        opened_connections: openedConections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
